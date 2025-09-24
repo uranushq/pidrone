60f2f5b0ec04ec2d54537406e4dd6641429ea311
directorbae <bjw020615@gmail.com>
2025-09-24 18:12:52 +0900

    luminance updated

--- src/rpi_play.cpp
+++ src/rpi_play.cpp
@@ -246,7 +246,7 @@ int main(int argc, char* argv[]) {
 
         const auto& frames = binDataMap[entry.filename];
         
-        const int interval_us = 30'000;
+        const int interval_us = 15'000;
         struct timespec nextFrameTime;
         clock_gettime(CLOCK_MONOTONIC, &nextFrameTime);
         
